f7fd64b811ddde1c23dad350a43e768e1f5ee725
Niel McLaren <d@nielmclaren.com>
2026-01-31 08:21:11 -0800

    Added a goal that can be reached/scored.

--- game.gd
+++ game.gd
@@ -1,15 +1,18 @@
 class_name Game
 extends Node2D
 
-@onready var _mask_container: Node2D = %MaskContainer
 @onready var _player_container: Node2D = %PlayerContainer
+@onready var _goal_container: Node2D = %GoalContainer
+@onready var _mask_container: Node2D = %MaskContainer
 @onready var _tilemap: TileMapLayer = %TileMapLayer
 
 var _mask_scene: PackedScene = load("res://game_objects/mask.tscn")
+var _goal_scene: PackedScene = load("res://game_objects/goal.tscn")
 var _player_scene: PackedScene = load("res://characters/player.tscn")
 
 var _players: Array[Player]
 var _masks: Array[Mask]
+var _goal: Goal
 
 
 func _ready() -> void:
@@ -20,6 +23,10 @@ func reset() -> void:
 	for player: Player in _players:
 		player.queue_free()
 	_players.clear()
+
+	if _goal:
+		_goal.queue_free()
+		_goal = null
 
 	for mask: Mask in _masks:
 		mask.queue_free()
@@ -36,24 +43,68 @@ func reset() -> void:
 			var tile: Vector2i = tiles.pop_back()
 			_tilemap.set_cell(Vector2i(c, r), 1, tile)
 
-	var player0: Player = _player_scene.instantiate()
-	player0.global_position = _tilemap.map_to_local(Vector2i(0, 3))
-	player0.player_num = 0
-	_player_container.add_child(player0)
-	_players.append(player0)
+	_spawn_player(0, Vector2i(0, 0))
+	_spawn_player(1, Vector2i(0, 3))
 
-	var player1: Player = _player_scene.instantiate()
-	player1.player_num = 1
-	player1.global_position = _tilemap.map_to_local(Vector2i(3, 0))
-	_player_container.add_child(player1)
-	_players.append(player1)
+	_spawn_goal()
 
 	for i: int in range(Constants.NUM_MASKS):
-		var coords: Vector2i = Vector2i(randi() % Constants.NUM_COLS, randi() % Constants.NUM_ROWS)
-		var mask: Mask = _mask_scene.instantiate()
-		_mask_container.add_child(mask)
-		_masks.append(mask)
-		mask.global_position = _tilemap.map_to_local(coords)
+		_spawn_mask()
+
+
+func _spawn_player(player_num: int, coord: Vector2i) -> void:
+	var player: Player = _player_scene.instantiate()
+	player.player_num = player_num
+	player.global_position = _tilemap.map_to_local(coord)
+	_player_container.add_child(player)
+	_players.append(player)
+
+
+func _spawn_goal() -> void:
+	var coord: Vector2i = Vector2i(3, randi() % Constants.NUM_ROWS)
+	var goal: Goal = _goal_scene.instantiate()
+	goal.tree_exited.connect(_goal_tree_exited)
+	goal.scored.connect(_goal_scored)
+	goal.global_position = _tilemap.map_to_local(coord)
+	_goal_container.add_child(goal)
+	_goal = goal
+
+
+func _goal_tree_exited() -> void:
+	_goal = null
+
+
+func _goal_scored(player: Player) -> void:
+	print("Player %d scored!" % player.player_num)
+
+
+func _spawn_mask() -> void:
+	var coord: Vector2i = Vector2i(randi() % Constants.NUM_COLS, randi() % Constants.NUM_ROWS)
+	var mask: Mask = _mask_scene.instantiate()
+	mask.tree_exited.connect(_mask_tree_exited.bind(mask))
+	mask.global_position = _tilemap.map_to_local(coord)
+	_mask_container.add_child(mask)
+	_masks.append(mask)
+
+
+func _mask_tree_exited(mask: Mask) -> void:
+	_masks.erase(mask)
+	_spawn_mask()
+
+
+func _is_coord_empty(coord: Vector2i) -> bool:
+	for player: Player in _players:
+		if _tilemap.local_to_map(player.global_position) == coord:
+			return false
+
+	for mask: Mask in _masks:
+		if _tilemap.local_to_map(mask.global_position) == coord:
+			return false
+
+	if _goal and _tilemap.local_to_map(_goal.global_position) == coord:
+		return false
+
+	return true
 
 
 func _input(event: InputEvent) -> void:
