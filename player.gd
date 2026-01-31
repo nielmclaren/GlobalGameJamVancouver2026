c37c9475da25470853b04852cc4fff806b73f415
Niel McLaren <d@nielmclaren.com>
2026-01-30 20:29:02 -0800

    Make two players move.

---
+++ player.gd
@@ -0,0 +1,15 @@
+class_name Player
+extends CharacterBody2D
+
+const SPEED: float = 20000
+
+@export var player_num: int = 0
+
+func _process(delta: float) -> void:
+	var dir: Vector2 = _get_input_vector()
+	velocity = dir * SPEED * delta
+	move_and_slide()
+
+
+func _get_input_vector() -> Vector2:
+	return Input.get_vector("move_left%d" % player_num, "move_right%d" % player_num, "move_up%d" % player_num, "move_down%d" % player_num)
